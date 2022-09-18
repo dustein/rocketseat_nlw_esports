5870600d0848dee38604b7a5558321d0a6a5f28d
DuStein <steindu@gmail.com>
2022-09-17 21:44:13 -0300

    tentando retomar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import './styles/main.css';
+import logoImage from './assets/logo.svg'
 
 function App() {
 
     return (
-      <div className="w-8 h-8 bg-violet-500"></div>
+      <div className="w-8 h-8 bg-violet-500">
+        <img src={logoImage} alt="" />
+      </div>
       )
 
 
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        universo: "url('/bg-universo.jpg')"
+      }
+    },
   },
   plugins: [],
 }
