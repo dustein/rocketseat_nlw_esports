10c312b6a2ac0daf0480cd68ea4d55ee6142d88e
DuStein <steindu@gmail.com>
2022-09-21 11:50:41 -0300

    pausando aula mobile not good

---
+++ mobile/PASSOSmobile.md
@@ -0,0 +1,3 @@
+No app.json configura o icone da aplicacao e a tela de splash
+Para criacao, visite "expo splash csreen" no google.
+Usaremos a pasta src para separar os arquivos que vamos criar, dos que já são padrão da aplicação.
---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,11 @@
+import { ImageBackground } from 'react-native';
+
+import { styles } from './styles';
+
+export function Background() {
+  return (
+    <ImageBackground style={styles.container}>
+
+    </ImageBackground>
+  );
+}
