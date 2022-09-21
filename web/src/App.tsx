b5755658b036ae3e775c4d87eabd4f0b668e4fb8
DuStein <steindu@gmail.com>
2022-09-20 22:10:27 -0300

    trabalhando app web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,8 +4,38 @@ import logoImage from './assets/logo.svg'
 function App() {
 
     return (
-      <div className="w-8 h-8 bg-violet-500">
+      <div className="max-w-[500px] mx-auto flex flex-col items-center my-10">
         <img src={logoImage} alt="" />
+
+        <h1 className='text-2xl text-white font-black mt-10'>Projeto <span className='text-yellow-500'>Treinamento</span> NLW eSports</h1>
+
+        <div className='grid grid-cols-6 gap-4 mt-12'>
+
+          <a href="">
+            <img src="/game-1.png" alt="" />
+          </a>
+
+          <a href="">
+            <img src="/game-2.png" alt="" />
+          </a>
+
+          <a href="">
+            <img src="/game-3.png" alt="" />
+          </a>
+
+          <a href="">
+            <img src="/game-4.png" alt="" />
+          </a>
+
+          <a href="">
+            <img src="/game-5.png" alt="" />
+          </a>
+
+          <a href="">
+            <img src="/game-6.png" alt="" />
+          </a>
+
+        </div>
       </div>
       )
 
