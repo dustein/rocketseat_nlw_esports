4b93a4db1862f3b8e6815cf646fb6e44cdfb8db6
dustein <steindu@gmail.com>
2022-09-23 15:50:12 -0300

    pausando work

--- src/server.ts
+++ src/server.ts
@@ -50,7 +50,34 @@ app.get('/games/:id/ads', async (req, res) => {
     
   })
 
-  return res.json(ads)
+
+  // return res.json(ads)
+  //formatar a resp para exbir os nueros do array do weekdays um em cada linha
+  return res.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+    }
+  }))
+})
+
+//rota para exibir o campo discord
+app.get('/ads/:id/discord', async (req, res) => {
+  const adId = req.params.id;
+  const ad = await prisma.ad.findUniqueOrThrow({
+    
+    select: {
+      discord: true,
+    },
+    
+    where: {
+      id: adId,
+    }
+  })
+
+  return res.json({
+    discord: ad.discord,
+  })
 })
 
 app.get('/', (req, res) => {
@@ -58,6 +85,28 @@ app.get('/', (req, res) => {
   res.send('acessou GET root')
 })
 
+app.post('/games/:id/ads', async (req, res) => {
+  const gameId = req.params.id;
+  const body = req.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: body.hourStart,
+      hourEnd: body.hourEnd,
+      useVoiceChannel: body.useVoiceChannel
+    }
+  })
+
+
+  return res.json(body)
+
+
+})
 
 
 app.listen(3333, () => { console.log('Servidor ON !')})
