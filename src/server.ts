02956719c69611d48aa03750d7d4d5f01c316631
dustein <steindu@gmail.com>
2022-09-23 15:21:45 -0300

    rota ads nos games id omite disacord

--- src/server.ts
+++ src/server.ts
@@ -26,9 +26,28 @@ app.get('/games/:id/ads', async (req, res) => {
   const gameIdSelecionado = req.params.id;
 
   const ads = await prisma.ad.findMany({
+    
+    //para nao exibir o campo "discord" nesta rota, vamos selecionar apenas os campos que queremos retornar 
+    
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true
+    },
+
     where: {
       gameId : gameIdSelecionado,
+    },
+//ordenar pela data de criacao do ad
+    orderBy: {
+      createdAt: 'desc'
     }
+
+    
   })
 
   return res.json(ads)
